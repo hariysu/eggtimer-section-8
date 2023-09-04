32d5fab635ce068baa96fdca2dbec6ad6591ca5b
Abdurahman AYDIN <hariysu@gmail.com>
2023-09-04 17:41:14 +0300

    Provides to show timer to user with a progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,65 +2,63 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
+//  Created by Abdurahman AYDIN on 04/09/2023.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
-//    let softTime = 5
-//    let mediumTime = 7
-//    let hardTime = 12
-    var secondsRemaining : Int?
-    var timer = Timer()
+    @IBOutlet weak var messageToUser: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
     
+    //var secondsRemaining : Int?
+    var timer = Timer()
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
     
-    @IBOutlet weak var messageToUser: UILabel!
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    var player: AVAudioPlayer!
+    
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        messageToUser.text = "How do you like your eggs?"
         
         let hardness = sender.currentTitle!
         
+        messageToUser.text = hardness
+        
         //print(eggTimes[hardness]!)
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        secondsPassed = 0
+        progressView.progress = 0.0
+        
         
         timer.invalidate()
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(UIMenuController.update), userInfo: nil, repeats: true)
         
-//        if hardness == "Soft" {
-//            print(softTime)
-//        }
-//        else if hardness == "Medium" {
-//            print(mediumTime)
-//        }
-//        else {
-//            print(hardTime)
-//        }
-        
-//        switch hardness {
-//        case "Soft":
-//            print(softTime)
-//        case "Medium":
-//            print(mediumTime)
-//        case "Hard":
-//            print(hardTime)
-//        default:
-//            print("Error")
-//        }
     }
     
     @objc func update() {
-        if(secondsRemaining! > 0) {
-            print("\(secondsRemaining!) seconds")
-            secondsRemaining! -= 1
+        if(totalTime > secondsPassed) {
+            secondsPassed += 1
+            progressView.progress = Float(secondsPassed) / Float(totalTime)
         }else {
             messageToUser.text = "Done"
             timer.invalidate()
+            secondsPassed = 0
+            playSound()
         }
     }
+    
+    func playSound() {
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
 }
 
